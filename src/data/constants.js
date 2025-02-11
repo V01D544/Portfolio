03c3f520a81702200d4f1906ddb4af9ba848935b
KUMAR SHUBHAM <93920333+V01D544@users.noreply.github.com>
2025-02-11 17:27:38 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,8 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/V01D544",
   resume:
-    "https://drive.google.com/file/d/1hXf4g5OensVMspFimefgXoOSawxwOqhq/view?usp=sharing",
-    // "https://drive.google.com/file/d/1KHqftYpPMTyqhMecBg_NHwNCMiJGsPiR/view?usp=share_link",
+   "https://drive.google.com/file/d/1i8iYx8Na8Z7yx_dFmqSEdnYBgSfSAKCV/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/krshubham544/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/i_krshubham544/",
